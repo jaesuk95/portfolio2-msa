cc6fde6ddd77d20bbd5a0a4101f0d4188f4b28e9
Jaesuk <jaesuk95@outlook.com>
2023-05-15 04:56:52 +0900

    Eureka Server 등록

--- config-service/src/main/java/com/portfolio/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/portfolio/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.portfolio.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer        // config 서버 역할
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
