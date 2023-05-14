91fc3b95a600356cc72701c0b3cba0aa120d1aa3
Jaesuk <jaesuk95@outlook.com>
2023-05-15 05:55:07 +0900

    api gateway test (git config <-> app)

---
+++ user-service/src/main/java/com/portfolio/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.portfolio.userservice.controller;
+
+import io.micrometer.core.annotation.Timed;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user-service")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Environment env;
+
+    @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true) // prometheus 에 등록
+    public String status(HttpServletRequest request) {
+        return String.format("It's working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", with token secret=" + env.getProperty("token.secret")
+                + ", with token time=" + env.getProperty("token.expiration_time"));
+    }
+
+}
