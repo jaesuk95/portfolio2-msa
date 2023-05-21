b0cf56d1d3b827df687cbe53b4ada56f9d07abc5
Jaesuk <jaesuk95@outlook.com>
2023-05-22 03:15:13 +0900

    order-service gradle -> maven project

--- order-service/src/main/java/com/portfolio/orderservice/message/kafka/producer/KafkaProducer.java
+++ order-service/src/main/java/com/portfolio/orderservice/message/kafka/producer/KafkaProducer.java
@@ -1,4 +1,4 @@
-//package com.portfolio.orderservice.message.kafka.producer;
+package com.portfolio.orderservice.message.kafka.producer;//package com.portfolio.orderservice.message.kafka.producer;
 //
 //import com.fasterxml.jackson.core.JsonProcessingException;
 //import com.fasterxml.jackson.databind.ObjectMapper;
