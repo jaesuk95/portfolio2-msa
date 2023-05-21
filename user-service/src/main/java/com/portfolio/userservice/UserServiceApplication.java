02c2798a226f5d5ee2319c5a1abf97519102235b
Jaesuk <jaesuk95@outlook.com>
2023-05-21 16:17:36 +0900

    discovery-service gradle -> maven + minikube deployments error fixes

--- user-service/src/main/java/com/portfolio/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/portfolio/userservice/UserServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
@@ -16,6 +18,11 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
