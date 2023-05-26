d5e5500552929ffa520641afdf4a7de1fc03856c
Jaesuk <jaesuk95@outlook.com>
2023-05-27 06:15:11 +0900

    product-service 만 왜 안되는지 해결중

--- product-service/src/main/java/com/portfolio/productservice/config/KafkaConsumerConfig.java
+++ product-service/src/main/java/com/portfolio/productservice/config/KafkaConsumerConfig.java
@@ -1,7 +1,8 @@
 package com.portfolio.productservice.config;
 
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -15,15 +16,13 @@ import java.util.HashMap;
 @Configuration
 public class KafkaConsumerConfig {
 
-//    @Value("${app.kafkaurl}")
-//    private String kafkaAddress;
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         HashMap<String, Object> properties = new HashMap<>();
-        // kafka container host
-//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.4:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
--- product-service/src/main/java/com/portfolio/productservice/config/KafkaProducerConfig.java
+++ product-service/src/main/java/com/portfolio/productservice/config/KafkaProducerConfig.java
@@ -1,6 +1,8 @@
 package com.portfolio.productservice.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,16 +16,12 @@ import java.util.HashMap;
 @Configuration
 public class KafkaProducerConfig {
 
-    // kafka producer configuration
-//    @Value("${app.kafkaurl}")
-//    private String kafkaAddress;
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
     @Bean
     public ProducerFactory<String,String> producerFactory() {
         HashMap<String, Object> configProps = new HashMap<>();
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.4:9092");
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.107.54.84:9092");
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-service:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
